82107fea7a5de629318ed2af898294178b4e76b0
nikhilnimapinfo <166018854+nikhilnimapinfo@users.noreply.github.com>
2024-07-11 17:42:38 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncControllerProgram.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        AsyncDBOEntities db = new AsyncDBOEntities();
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            var list=await db.Employee.ToListAsync();
+
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employee.Add(employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+    }
+}
